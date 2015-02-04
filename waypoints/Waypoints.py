fixed waypoint trajectory computation; add SensorConfiguration class and LinkPath class for new sensor submodule

waypoints/Waypoints.py
@@ -218,12 +218,12 @@ class Waypoints:
         n: number of points to take on the line
         """
         x1,y1,x2,y2 = directed_line
-        for k,t in enumerate(np.linspace(0,1,n)):
-            if k == 0: ids = [self.closest_to_point((x1,y1), fast)]
-            if k > 0:
-                id = self.closest_to_point((x1+t*(x2-x1), y1+t*(y2-y1)), fast)
-                if id != ids[-1]: ids.append(id)
-        return ids
+        interp_x = np.linspace(x1,x2,num=n)
+        interp_y = np.linspace(y1,y2,num=n)
+        ids = [self.closest_to_point((x,y), fast) for (x,y) in zip(interp_x,interp_y)]
+        ids_deduped = [ids[0]]
+        ids_deduped.extend([y for (x,y) in zip(ids,ids[1:]) if x!=y])
+        return ids_deduped
 
 
     def closest_to_polyline(self, polyline, n, fast=False):
@@ -234,14 +234,11 @@ class Waypoints:
         polyline: list of directed lines [(x1,y1,x2,y2)]
         n: number of points to take on each line of the polyline
         """
-        ids = []
-        for k, line in enumerate(polyline):
-            if k == 0: ids = self.closest_to_line(line, n, fast)
-            if k > 0:
-                tmp = self.closest_to_line(line, n, fast)
-                if ids[-1] == tmp[0]: ids += tmp[1:]
-                if ids[-1] != tmp[0]: ids += tmp
-        return ids
+        ids = [self.closest_to_line(line, n, fast) for line in polyline]
+        ids = [item for sublist in ids for item in sublist]
+        ids_deduped = [ids[0]]
+        ids_deduped.extend([y for (x,y) in zip(ids,ids[1:]) if x!=y])
+        return ids_deduped
 
 
     def closest_to_path(self, graph, path, n, fast=False):
@@ -284,6 +281,11 @@ class Waypoints:
         for value,key in enumerate(path_wps):
             wps.setdefault(tuple(key), []).append(value)
         return path_wps, wps
+
+    def _get_cp_trajs(self, graph, n, fast=False, tol=1e-3):
+        path_cps, cp_trajs = self.get_wp_trajs(graph.G,graph.routes,
+                                                  n, fast=fast, tol=tol)
+        return path_cps, cp_trajs
 
 # Helper functions
 # -------------------------------------